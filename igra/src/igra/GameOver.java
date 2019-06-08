a1645d8345cf5448f6fc4eb291488c63e92b1e19
Luka Otović <luka19@windowslive.com>
2019-06-08 18:44:36 +0200

    gameover

---
+++ igra/src/igra/GameOver.java
@@ -0,0 +1,54 @@
+package igra;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import Engine.CenterCamera;
+import Engine.EngineCore;
+import Engine.EngineEpisode;
+import Engine.ImageContainer;
+import Engine.StaticImage;
+
+public class GameOver extends EngineEpisode{
+	public float SCALE_FACTOR = 1f;
+	
+	@Override
+	public void mainLoop(long renderDeltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(int key, int state) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void init(EngineCore engineCore) {
+		CenterCamera camera = new CenterCamera();
+		this.setCameraController(camera);
+		
+		ImageContainer container = new ImageContainer(engineCore);
+		this.addViewComponent(container);
+		
+		
+		Image img0=null;
+		try {
+			img0 = ImageIO.read(new File("../Assets/game_over.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		int posx = engineCore.getWidth()/2 - (int)(img0.getWidth(null)*SCALE_FACTOR/2);
+		int posy = engineCore.getHeight()/2 - (int)(img0.getHeight(null)*SCALE_FACTOR/2);
+		
+		StaticImage menu = new StaticImage(engineCore, img0, posx, posy);
+		container.add(menu);
+	}
+
+}
