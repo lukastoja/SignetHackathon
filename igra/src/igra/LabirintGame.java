75bbc1be290bf55b4698607ef623b2895847dc21
Ivana Zuzic <izuzic3@gmail.com>
2019-06-08 14:20:58 +0200

    Random generated maze

--- igra/src/igra/LabirintGame.java
+++ igra/src/igra/LabirintGame.java
@@ -1,13 +1,15 @@
 package igra;
 
+import java.util.Random;
+
 import Engine.EngineCore;
 import Engine.EngineEpisode;
 import Engine.Grid;
 import Engine.StaticCamera;
 
 public class LabirintGame extends EngineEpisode{
-	public static final int MAZE_WIDTH = 40;
-	public static final int MAZE_HEIGHT = 40;
+	public static final int MAZE_WIDTH = 10;
+	public static final int MAZE_HEIGHT = 10;
 	Grid labirintGrid;
 	
 	@Override
@@ -24,14 +26,36 @@ public class LabirintGame extends EngineEpisode{
 
 	@Override
 	public void init(EngineCore engineCore) {
+		
+		int h = MAZE_HEIGHT * 2 + 1;
+		int w = MAZE_WIDTH * 4 + 1;
+		
 		StaticCamera camera = new StaticCamera(0, 0);
 		this.setCameraController(camera);
 		
-		labirintGrid = new Grid(engineCore, 64, 64, MAZE_WIDTH, MAZE_HEIGHT);
+		labirintGrid = new Grid(engineCore, 64, 64, w, h);
 		this.addViewComponent(labirintGrid);
+		generirajLabirint(labirintGrid);
 		
 		labirintGrid.setDistanceFromCamera(0.2f);
 		
 	}
-
+	
+	private void generirajLabirint(Grid labirintGrid) {	
+		
+		int h = MAZE_HEIGHT * 2 + 1;
+		int w = MAZE_WIDTH * 4 + 1;
+		
+		MyMaze maze = new MyMaze(MAZE_WIDTH, MAZE_HEIGHT);
+	    char[][] grid = maze.getGrid();
+	    
+	    for(int i=0; i < grid.length; i++) {
+			for(int j=0; j < grid[i].length; j++) {
+				if(grid[i][j] == 'X') {
+					LabirintBlock blk = new LabirintBlock(labirintGrid, j, i);
+					labirintGrid.add(blk);
+				}
+			}
+		}
+	}
 }
