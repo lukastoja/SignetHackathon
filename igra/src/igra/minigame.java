cc638a8eec6d2168b17b3c09c29632091820cae6
ecokeco <kecoeco@gmail.com>
2019-06-08 11:12:55 +0200

    Starting class

--- igra/src/igra/minigame.java
+++ igra/src/igra/minigame.java
@@ -1,9 +1,19 @@
 package igra;
 
+import Engine.EngineCore;
+
 public class minigame {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		EngineCore engine = new EngineCore(640, 480, false);
+		
+		PackmanGame game = new PackmanGame();
+		game.setEngine(engine);
+		
+		engine.setEpisode(game);
+		//engine.hideCursor();
+		engine.start();
+
 
 	}
 
