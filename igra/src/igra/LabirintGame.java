96d274a74902a1170e54cd2ffa828d1c736e5f15
Ivana Zuzic <izuzic3@gmail.com>
2019-06-08 15:26:11 +0200

    Attempt to make far away blocks transparent

--- igra/src/igra/LabirintGame.java
+++ igra/src/igra/LabirintGame.java
@@ -17,10 +17,11 @@ public class LabirintGame extends EngineEpisode{
 	public static final int MAZE_WIDTH = 10;
 	public static final int MAZE_HEIGHT = 10;
 	final static float SPEED = 5.5f;
+	private int w, h;
 	Grid labirintGrid;
 	LabirintIgrac player;
 	LabirintShip ship;
-	ArrayList<LabirintBlock> blocks;
+	ArrayList<LabirintBlock> blocks = new ArrayList();
 
 	@Override
 	public void mainLoop(long renderDeltaTime) {
@@ -45,8 +46,8 @@ public class LabirintGame extends EngineEpisode{
 	@Override
 	public void init(EngineCore engineCore) {
 		
-		int h = MAZE_HEIGHT * 2 + 1;
-		int w = MAZE_WIDTH * 4 + 1;
+		h = MAZE_HEIGHT * 2 + 1;
+		w = MAZE_WIDTH * 4 + 1;
 		
 		labirintGrid = new Grid(engineCore, 64, 64, w, h);
 		this.addViewComponent(labirintGrid);
@@ -67,9 +68,6 @@ public class LabirintGame extends EngineEpisode{
 	
 	private void generirajLabirint(Grid labirintGrid) {	
 		
-		int h = MAZE_HEIGHT * 2 + 1;
-		int w = MAZE_WIDTH * 4 + 1;
-		
 		MyMaze maze = new MyMaze(MAZE_WIDTH, MAZE_HEIGHT);
 	    char[][] grid = maze.getGrid();
 	    
@@ -78,7 +76,7 @@ public class LabirintGame extends EngineEpisode{
 				if(grid[i][j] == 'X') {
 					LabirintBlock blk = new LabirintBlock(labirintGrid, j, i);
 					labirintGrid.add(blk);
-					//blocks.add(blk);
+					blocks.add(blk);
 				}
 			}
 		}
@@ -91,15 +89,34 @@ public class LabirintGame extends EngineEpisode{
 		bindKey(KeyEvent.VK_RIGHT, "key_right");
 	}
 	
+	private double distanceBetweenPoints(
+	  double x1, 
+	  double y1, 
+	  double x2, 
+	  double y2) {       
+	    return Math.sqrt((y2 - y1) * (y2 - y1) + (x2 - x1) * (x2 - x1));
+	}
+	
 	private class PlayerStigao implements ActionCompleted{
 		@Override
 		public void actionCompleted() {
-			Position pos = player.getPosition();
+			Position posPlayer = player.getPosition();
 			Position posShip = ship.getPosition();
-			if (pos.getX() == posShip.getX()-1 && pos.getY() == posShip.getY()
-					|| pos.getX() == posShip.getX() && pos.getY() == posShip.getY()-1) {
+			int playerX = posPlayer.getX();
+			int playerY = posPlayer.getY();
+			if (playerX == posShip.getX()-1 && playerY == posShip.getY()
+					|| playerX == posShip.getX() && playerY == posShip.getY()-1) {
 				System.out.println("Stigao!");
 				System.exit(0); 
+			}
+			for (LabirintBlock blk: blocks) {
+				Position blkPos = blk.getPosition();
+				int blkPosX = blkPos.getX();
+				int blkPosY = blkPos.getY();
+				float dist = (float) distanceBetweenPoints(blkPosX, blkPosY, playerX, playerY);
+				float maxDist = (float) distanceBetweenPoints(0, 0, w, h);
+				System.out.println(dist / maxDist);
+				//blk.setTransparancy(0.5f);
 			}
 		}
 	}
